598adb58f096912fe2a16b8b1979673820da70e1
neerajshaw <neerajshaw91@gmail.com>
2025-06-04 01:22:41 -0700

    Added python.py file

---
+++ python.py
@@ -0,0 +1,13 @@
+#letter = '''Dear <Name>,
+#You are selected!
+#<Date>'''
+
+#print(letter.replace("<Name>", "Neeraj").replace("<Date>", "27 may"))
+
+#name = " Neeraj is a good  boy  and "
+
+#print(name.find("  "))
+var1 = 23
+var2 = 27
+add = var1+var2
+print(add)
